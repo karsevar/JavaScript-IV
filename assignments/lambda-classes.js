4ddd51d629f437e7bf2d221927d48cbf3d4886e1
masonkarsevar <masonkarsevar@gmail.com>
2019-06-13 12:07:37 -0700

    MVP: ProjectManager and Student classes finished. WIP: tests.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -27,3 +27,41 @@ class Instructor extends Person {
 		console.log(`${student.name} recieves a perfect score on ${subject}`);
 	}
 }
+
+class ProjectManager extends Instructor {
+	constructor(attributes) {
+		super(attributes);
+		this.gradClassName = attributes.gradClassName;
+		this.favInstructor = attributes.favInstructor;
+	}
+
+	standUp(channelName) {
+		console.log(`${this.name} announces to ${channelName}, @channel standy times!`)
+	}
+
+	debugCode(student, subject) {
+		console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+	}
+}
+
+class Student extends Person {
+	constructor(attributes) {
+		super(attributes);
+		this.className = attributes.className;
+		this.favSubjects = attributes.favSubjects;
+	}
+
+	listsSubjects() {
+		attributes.favSubjects.forEach(subject => console.log(subject));
+	}
+
+	PRAssignment(subject) {
+		console.log(`${this.student} has submitted a PR for ${subject}`);
+	}
+
+	sprintChallenge(subject) {
+		console.log(`${this.name} has begin sprint challenge on ${subject}`);
+	}
+}
+
+
